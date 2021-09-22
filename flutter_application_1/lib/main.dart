1126a938ce823bde6e9533f244f32ac3310db848
Laura-May <lauramay.g@hotmail.com>
2021-09-22 10:18:12 -0400

    modifications première partie du tp

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -74,6 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        backgroundColor: Colors.pink.shade300,
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -109,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.favorite),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
